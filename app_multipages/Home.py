0243b882f253b68ecab9411bbf19d752f350ed6f
Gustavo <gustavo.belini@hotmail.com>
2024-03-15 18:13:59 -0300

    Atualizado o caminho da imagem

--- app_multipages/Home.py
+++ app_multipages/Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title = "Predição de seguro de saúde",
-    page_icon = "img\stethoscope.png"
+    page_icon = "../img/stethoscope.png"
 )
 
 # st.sidebar.header('Descrição do projeto')
